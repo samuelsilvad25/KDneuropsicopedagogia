3e2ffb4b9cc713ebeeb55a0c1144083c4185ac24
samuelsilvad25 <samuelsilva.d25@gmail.com>
2026-03-29 22:49:31 -0300

    adiciona textos, imagens e animações

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const ano = new Date().getFullYear();
 document.getElementById("ano").textContent = ano;
+
+const elementos = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('ativo');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elementos.forEach(el => observer.observe(el));
